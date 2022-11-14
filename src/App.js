1f0401f39fbb0e1730f36d47080e9fddcc286a21
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-11-14 23:18:23 +0400

    add_reset

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
+export default class App extends Component {
+  state = {
+    value: "",
+    name:null
+  };
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+    console.log(this.state);
+  };
+  reset=()=>{
+    this.setState({name:""})
+  }
 
-export default class App extends Component{
-  state={
-   value:""
-  }
-  handleChange = (event)=>{
-    this.setState({value:event.target.value})
-    console.log(this.state)
-  }
-  
-  render(){
-    return(
+  render() {
+    return (
       <>
-      <label htmlFor="name">Name: 
-      <input 
-      id="name"
-      name="name"
-      onChange={this.handleChange}/>
-      </label>
-      <br/>
-      <label htmlFor="name">E-mail: 
-      <input 
-      id="name"
-      name="name"
-      onChange={this.handleChange}/>
-      </label>
-      <br/>
-      <label htmlFor="name">Password: 
-      <input 
-      id="name"
-      name="name"
-      type="password"
-      onChange={this.handleChange}/>
-      </label>
-      <p>{this.state.value}</p>
+        <label htmlFor="name">
+          Name:
+          <input
+            value={this.state.name}
+            id="name"
+            name="name"
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="button" onClick={this.reset} >
+          X
+        </button>
+        <br />
+        <label htmlFor="name">
+          E-mail:
+          <input 
+          value={this.state.name}
+          
+          id="name" name="name" onChange={this.handleChange} />
+        </label>
+        <button type="button" onClick={this.reset} >X</button>
+        <br />
+        <label htmlFor="name">
+          Password:
+          <input
+          value={this.state.name}
+            id="name"
+            name="name"
+            type="password"
+            onChange={this.handleChange}
+          />
+         <button type="button" onClick={this.reset} >X</button>
+        </label>
+        <p>{this.state.value}</p>
       </>
-    )
+    );
   }
 }
